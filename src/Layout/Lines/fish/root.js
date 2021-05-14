ec26e8ed1727837b14e82253e7a73c216f8b50dc
Miao Zhuang <1060950782@163.com>
2021-05-14 17:51:40 +0800

    add fish

---
+++ src/Layout/Lines/fish/root.js
@@ -0,0 +1,41 @@
+function drawRoot(item){
+     const {rect,contentRect} = item;
+     const {direction} = this;
+     const { minFishWidth = 200,minFishHeight = 50,angle,fishTailWidth = 50,headGap = 50} = this.remind.options.fish;
+        
+     const color = item.getColor();
+     const startPos = {
+        x:item.x + (direction === 'right' ? 0 : -contentRect.width),
+        y:item.y + contentRect.height / 2,
+        type:"moveTo"
+    }
+    const endPos = {
+        x:(direction === 'right' ? (item.globalPos.x + fishTailWidth) : (item.globalPos.x + rect.width - fishTailWidth)),
+        y:startPos.y
+    };
+     const bodyData = {
+         path:[startPos,endPos],
+         color
+     }
+     
+     this.page.lines.push(bodyData);
+
+     //尾巴
+     const startX = endPos.x + (direction === 'right' ? -fishTailWidth : fishTailWidth);
+     const disY = fishTailWidth * Math.tan(angle / 2);
+     this.page.lines.push({
+        path:[{
+            type:'moveTo',
+            x:startX,
+            y:endPos.y - disY,
+        },{
+           ...endPos
+        },{
+            x:startX,
+            y:endPos.y + disY,
+        }],
+        color
+     });
+}
+// 不得使用箭头函数
+export default drawRoot
