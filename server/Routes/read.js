cbb46583f9aec399ae63b0bb953a16c73ea83ffd
Miao Zhuang <1060950782@163.com>
2021-06-18 11:37:13 +0800

    ok

--- server/Routes/read.js
+++ server/Routes/read.js
@@ -2,6 +2,21 @@ const fs = require('fs');
 const qs = require("qs");
 
 module.exports = (req,res,router)=>{
-    const path = router.path;
-    const data = fs.readFileSync(path)
+    const {url} = req;
+    const params = qs.parse(url.split('?')[1]);
+    res.statusCode = 200;
+    
+    try{
+        const data = fs.readFileSync(params.path);
+        res.end(JSON.stringify({
+            success:true,
+            data
+        }))
+    }catch(e){
+        res.end(JSON.stringify({
+            success:false,
+            message:e.message
+        }))
+    };
+    
 }
--- server/Routes/save.js
+++ server/Routes/save.js
@@ -1,8 +1,34 @@
+const fs = require("fs");
+const qs = require("qs");
+
 const save = async (req,res)=>{
-    res.statusCode = 200;
-    res.end(JSON.stringify({
-        success:true,
-        message:"保存成功"
-    }))
+    let body = ''; 
+    req.setEncoding('utf8');
+    const params = qs.parse(url.split('?')[1]);
+    if(!params.path){
+        return res.end(JSON.stringify({
+            success:false,
+            message:"请选择合法路径"
+        }))
+    }
+    req.on("data",chunk=>{
+        body+=chunk;
+    })
+    req.on('end', () => {
+        try {
+          // 响应信息给用户。
+          res.statusCode = 200;
+          fs.writeFileSync(params.path,body)
+          res.end(JSON.stringify({
+              success:true,
+              message:"保存成功"
+          }))
+        } catch (er) {
+          // json 解析失败。
+          res.statusCode = 400;
+          return res.end(`错误: ${er.message}`);
+        }
+      });
+   
 }
 module.exports = save;
